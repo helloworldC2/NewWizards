3ccf91150074a70021758876dfe26f688d57de8a
Adma20 <adam.cook13@hotmail.com>
2015-04-21 03:06:31 +0100

    item 3

--- item.js
+++ item.js
@@ -47,7 +47,99 @@ ItemSpade.prototype.use = function(x,y,z){
   }
 };
 function ItemAxe(id,name,image,power){
+  Item.call(this,id,name,image,1);
+  this.power = power;
+  this.durability = power*10;
+}
+ItemAxe.prototype.render = Item.prototype.render;
 
+ItemAxe.prototype.use = function(x,y,z){
+  var tile = level.getTile(x,y,z);
+  if(tile.tool=="axe"){
+    //console.log("Set tile at"+x+","+y+"z"+z+" to "+AIR);
+    level.incrementData(x,y,z,this.power+Math.floor((Math.random()*8)-4));
+    if(level.getData(x,y,z)>20){
+      level.setTile(x,y,z,itemLog);
+      for(var i=0;i<9;i++){
+        var dx = (i%3)-1;
+        var dy = Math.floor(i/3)-1;
+        if(level.getTile(x+dx,y+dy,z-1).isSolid){
+          level.setTile(x+dx,y+dy,z-1,stoneFloor);
+        }
+      }
+
+      level.setTile(x,y-1,z-1,steps);
+      var dirt = new EntityItem(itemDirt,x<<5,y<<5,z,(Math.random()*2)-1);
+      this.durability--;
+      if(this.durability<0)player.inventory.removeItem(this);
+    }
+  }
+};
+
+function ItemPickAxe(id,name,image,power){
+  Item.call(this,id,name,image,1);
+  this.power = power;
+  this.durability = power*10;
+}
+ItemPickAxe.prototype.render = Item.prototype.render;
+
+ItemPickAxe.prototype.use = function(x,y,z){
+  var tile = level.getTile(x,y,z);
+  if(tile.tool=="axe"){
+    //console.log("Set tile at"+x+","+y+"z"+z+" to "+AIR);
+    level.incrementData(x,y,z,this.power+Math.floor((Math.random()*8)-4));
+    if(level.getData(x,y,z)>20){
+      level.setTile(x,y,z,itemLog);
+      for(var i=0;i<9;i++){
+        var dx = (i%3)-1;
+        var dy = Math.floor(i/3)-1;
+        if(level.getTile(x+dx,y+dy,z-1).isSolid){
+          level.setTile(x+dx,y+dy,z-1,stoneFloor);
+        }
+      }
+
+      level.setTile(x,y-1,z-1,steps);
+      var dirt = new EntityItem(itemDirt,x<<5,y<<5,z,(Math.random()*2)-1);
+      this.durability--;
+      if(this.durability<0)player.inventory.removeItem(this);
+    }
+  }
+};
+
+
+function ItemBucket(id,name,image,power){
+  Item.call(this,id,name,image,1);
+  this.power = power;
+  this.durability = power*10;
+}
+ItemBucket.prototype.render = Item.prototype.render;
+
+ItemBucket.prototype.use = function(x,y,z){
+  var tile = level.getTile(x,y,z);
+  if(tile.tool=="axe"){
+    //console.log("Set tile at"+x+","+y+"z"+z+" to "+AIR);
+    level.incrementData(x,y,z,this.power+Math.floor((Math.random()*8)-4));
+    if(level.getData(x,y,z)>20){
+      level.setTile(x,y,z,itemLog);
+      for(var i=0;i<9;i++){
+        var dx = (i%3)-1;
+        var dy = Math.floor(i/3)-1;
+        if(level.getTile(x+dx,y+dy,z-1).isSolid){
+          level.setTile(x+dx,y+dy,z-1,stoneFloor);
+        }
+      }
+
+      level.setTile(x,y-1,z-1,steps);
+      var dirt = new EntityItem(itemDirt,x<<5,y<<5,z,(Math.random()*2)-1);
+      this.durability--;
+      if(this.durability<0)player.inventory.removeItem(this);
+    }
+  }
+};
 var items = [];
-var itemDirt = new Item(0,"Chunk of dirt",grassImg,4);
+var itemDirt = new Item(0,"Chunk of dirt",dirtImg,4);
 var woodenSpade = new ItemSpade(1,"Wooden Spade",sandImg,5);
+var woodenAxe = new ItemAxe(2,"Wooden axe",sandImg,5);
+var itemLog = new Item(3,"Chunk of log",logsImg);
+var woodenPickAxe = new ItemPickAxe(4,"Wooden Pickaxe",sandImg,5);
+var Bucket = new ItemBucket(5,"bucket",sandImg,5);
